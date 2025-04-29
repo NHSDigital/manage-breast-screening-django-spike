267dbf449dec329d8741d821a1f36afda5d00ef2
Colin Saliceti <8416694+saliceti@users.noreply.github.com>
2025-04-29 16:35:34 +0100

    wip

--- infrastructure/terraform/main_new.tf
+++ infrastructure/terraform/main_new.tf
@@ -7,5 +7,5 @@ module "container-app-environment" {
   name                       = "manage-breast-screening-${var.environment}"
   resource_group_name        = local.resource_group_name                  # TODO: recreate
   log_analytics_workspace_id = azurerm_log_analytics_workspace.example.id # TODO: recreate
-  vnet_integration_subnet_id = azurerm_subnet.example.id                  # TODO: recreate
+  vnet_integration_subnet_id = module.container_app_subnet.id
 }
---
+++ infrastructure/terraform/networking_new.tf
@@ -0,0 +1,13 @@
+module "container_app_subnet" {
+  source = "../modules/dtos-devops-templates/infrastructure/modules/subnet"
+
+  name                                                           = "container_app_subnet"
+  resource_group_name                                            = local.resource_group_name            # TODO: recreate
+  vnet_name                                                      = azurerm_virtual_network.example.name # TODO: recreate
+  address_prefixes                                               = ["10.0.0.0/23"]                      # TODO: could be default value?
+  create_nsg                                                     = false
+  location                                                       = "UK South"                                 # TODO: not required if not creating NSG
+  monitor_diagnostic_setting_network_security_group_enabled_logs = []                                         # TODO: not required if not creating NSG
+  log_analytics_workspace_id                                     = azurerm_log_analytics_workspace.example.id # TODO: recreate
+  network_security_group_name                                    = "container_app_subnet"                     # TODO: create default name?
+}
