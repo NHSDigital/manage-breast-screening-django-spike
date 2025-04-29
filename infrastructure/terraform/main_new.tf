aa1e0a4cc0858e2f1eb1b64f1a3148af7b0d8b3a
Colin Saliceti <8416694+saliceti@users.noreply.github.com>
2025-04-29 16:23:35 +0100

    wip

--- infrastructure/terraform/main_new.tf
+++ infrastructure/terraform/main_new.tf
@@ -3,9 +3,9 @@ variable "environment" {
 }
 
 module "container-app-environment" {
-  source = "../modules/dtos-devops-templates/infrastructure/modules/container-app-environment"
-  name = "manage-breast-screening-${var.environment}"
-  resource_group_name = local.resource_group_name # TODO: recreate
-  log_analytics_workspace_id = azurerm_log_analytics_workspace.example.id  # TODO: recreate
-  vnet_integration_subnet_id = azurerm_subnet.example.id
+  source                     = "../modules/dtos-devops-templates/infrastructure/modules/container-app-environment"
+  name                       = "manage-breast-screening-${var.environment}"
+  resource_group_name        = local.resource_group_name                  # TODO: recreate
+  log_analytics_workspace_id = azurerm_log_analytics_workspace.example.id # TODO: recreate
+  vnet_integration_subnet_id = azurerm_subnet.example.id                  # TODO: recreate
 }
