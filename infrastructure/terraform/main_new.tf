71a24c6dfcf1c95ee706d0264071edd72faa3390
Colin Saliceti <8416694+saliceti@users.noreply.github.com>
2025-04-29 16:12:50 +0100

    module

---
+++ infrastructure/terraform/main_new.tf
@@ -0,0 +1,11 @@
+variable "environment" {
+  description = "Application environment name"
+}
+
+module "container-app-environment" {
+  source = "../modules/dtos-devops-template/container-app-environment"
+  name = "manage-breast-screening-${var.environment}"
+  resource_group_name = local.resource_group_name # TODO: recreate
+  log_analytics_workspace_id = azurerm_log_analytics_workspace.example.id  # TODO: recreate
+  vnet_integration_subnet_id = azurerm_subnet.example.id
+}
