b3508374bf306ee8fbd1522f738dfefe973d0367
Colin Saliceti <8416694+saliceti@users.noreply.github.com>
2025-04-30 11:42:12 +0100

    wip

--- infrastructure/terraform/main_new.tf
+++ infrastructure/terraform/main_new.tf
@@ -9,3 +9,16 @@ module "container-app-environment" {
   log_analytics_workspace_id = azurerm_log_analytics_workspace.example.id # TODO: recreate
   vnet_integration_subnet_id = module.container_app_subnet.id
 }
+
+module "webapp" {
+  source                       = "../modules/dtos-devops-templates/infrastructure/modules/container-app"
+  name                         = "manage-breast-screening-web-${var.environment}"
+  container_app_environment_id = module.container_app_environment.id
+  resource_group_name          = local.resource_group_name
+  app_key_vault_name           = "kv-colin-spike"
+  docker_image                 = var.docker_image
+  environment_variables = {
+    "ALLOWED_HOSTS" = "manage-breast-screening-django.${module.container-app-environment.default_domain}"
+  }
+  http_port = 8000
+}
