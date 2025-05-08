aea4576ec3de448283e8cfd7e445fd1e612ed6d3
Colin Saliceti <8416694+saliceti@users.noreply.github.com>
2025-05-08 16:39:55 +0100

    wip

--- infrastructure/terraform/networking_new.tf
+++ infrastructure/terraform/networking_new.tf
@@ -13,8 +13,8 @@ module "main_vnet" {
 }
 
 data "azurerm_private_dns_resolver" "this" {
-  name                = "${hub}-uks-hub-private-dns-zone-resolver"
-  resource_group_name = "rg-hub-${hub}-uks-private-dns-zones"
+  name                = "${var.hub}-uks-hub-private-dns-zone-resolver"
+  resource_group_name = "rg-hub-${var.hub}-uks-private-dns-zones"
 }
 
 data "azurerm_private_dns_resolver_inbound_endpoint" "this" {
