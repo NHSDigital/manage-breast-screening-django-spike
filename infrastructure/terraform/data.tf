8d0f8df9d27f0a41f713bccfa04f085b8d9d8be3
Colin Saliceti <8416694+saliceti@users.noreply.github.com>
2025-05-09 15:41:14 +0100

    wip

--- infrastructure/terraform/data.tf
+++ infrastructure/terraform/data.tf
@@ -1,9 +1,9 @@
 # TODO: create KV with terraform
-data "azurerm_key_vault" "app" {
-  name                = "kv-colin-spike"
-  resource_group_name = var.resource_group_name
-}
+# data "azurerm_key_vault" "app" {
+#   name                = "kv-colin-spike"
+#   resource_group_name = var.resource_group_name
+# }
 
-data "azurerm_key_vault_secrets" "app" {
-  key_vault_id = data.azurerm_key_vault.app.id
-}
+# data "azurerm_key_vault_secrets" "app" {
+#   key_vault_id = data.azurerm_key_vault.app.id
+# }
--- infrastructure/terraform/main_new.tf
+++ infrastructure/terraform/main_new.tf
@@ -27,7 +27,7 @@ module "container-app-environment" {
   source = "../modules/dtos-devops-templates/infrastructure/modules/container-app-environment"
 
   name                       = "manage-breast-screening-${var.environment}"
-  resource_group_name        = var.resource_group_name                    # TODO: recreate
+  resource_group_name        = azurerm_resource_group.main                    # TODO: recreate
   log_analytics_workspace_id = azurerm_log_analytics_workspace.example.id # TODO: recreate
   vnet_integration_subnet_id = module.container_app_subnet.id
 }
@@ -36,8 +36,9 @@ module "webapp" {
   source                       = "../modules/dtos-devops-templates/infrastructure/modules/container-app"
   name                         = "manage-breast-screening-web-${var.environment}"
   container_app_environment_id = module.container-app-environment.id
-  resource_group_name          = var.resource_group_name
-  app_key_vault_name           = "kv-colin-spike"
+  resource_group_name          = azurerm_resource_group.main
+  # app_key_vault_name           = "kv-colin-spike"
+  app_key_vault_name           = module.app-key-vault.name
   docker_image                 = var.docker_image
   environment_variables = {
     "ALLOWED_HOSTS" = "manage-breast-screening-web-${var.environment}.${module.container-app-environment.default_domain}"
