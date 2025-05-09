649e8e334c9f074065fba7453a06d43e59376bd2
Colin Saliceti <8416694+saliceti@users.noreply.github.com>
2025-05-09 19:52:45 +0100

    wip

--- infrastructure/terraform/main_new.tf
+++ infrastructure/terraform/main_new.tf
@@ -40,7 +40,7 @@ module "webapp" {
   resource_group_name          = azurerm_resource_group.main.name
   # app_key_vault_name           = "kv-colin-spike"
   # fetch_secrets_from_app_key_vault = true
-  app_key_vault_name           = module.app-key-vault.name
+  app_key_vault_name           = module.app-key-vault.key_vault_name
   docker_image                 = var.docker_image
   environment_variables = {
     "ALLOWED_HOSTS" = "manage-breast-screening-web-${var.environment}.${module.container-app-environment.default_domain}"
