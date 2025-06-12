3aa01520c46baee3e4114f2aaa03106a9b505a2a
Mat Moore <MatMoore@users.noreply.github.com>
2025-06-12 16:19:10 +0100

    wip

--- manage_breast_screening/core/models.py
+++ manage_breast_screening/core/models.py
@@ -29,14 +29,18 @@ class AuditLog(models.Model):
 
     id = models.UUIDField(default=uuid.uuid4, editable=False, primary_key=True)
     created_at = models.DateTimeField(auto_now_add=True)
-    content_type = models.ForeignKey(ContentType, on_delete=models.SET_NULL, null=True)
+    content_type = models.ForeignKey(ContentType, on_delete=models.PROTECT, null=True)
     object_id = models.UUIDField()
     content_object = GenericForeignKey("content_type", "object_id")
     operation = models.CharField(choices=OperationChoices)
     changes = models.JSONField()
     actor = models.ForeignKey(
-        settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True
+        settings.AUTH_USER_MODEL, on_delete=models.PROTECT, null=True
     )
+    system_update_id = models.CharField(null=True)
+
+    def __str__(self):
+        return f"{self.get_operation_display()} {self.content_type} ({self.object_id})"
 
     @classmethod
     def log_actions(cls, objects, operation, actor):
